aa0d5ec0e495ebef8239b10189023456011783f4
woozie <fawzejabbour@gmail.com>
2026-02-15 06:01:30 +0300

    Initial commit

---
+++ src/main/java/com/fawzi/org/bustrips/BusTripsApplication.java
@@ -0,0 +1,13 @@
+package com.fawzi.org.bustrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTripsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fawzi/org/bustrips/BusTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fawzi.org.bustrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
